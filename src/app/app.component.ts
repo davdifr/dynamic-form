57b28bc1e2c06206d7a48334fce92c10903828d0
Davide Di Francesco <davdifr@outlook.com>
2024-05-25 23:11:56 +0200

    chore: Add dictionary interface and tailwind configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
+import { Dictionary } from './models/dictionary.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [DynamicFormComponent],
+  template: `
+    <div class="p-8">
+      <h2 class="mb-4 text-xl font-bold">Persona Form</h2>
+      <app-dynamic-form
+        [initialData]="persona"
+        (formValues)="onFormValues($event)"
+      />
+
+      <h2 class="mt-8 mb-4 text-xl font-bold">Contratto Form</h2>
+      <app-dynamic-form
+        [initialData]="contratto"
+        (formValues)="onFormValues($event)"
+      />
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'dynamic-form';
+  title = 'dynamic-forms';
+
+  onFormValues(values: Dictionary) {
+    console.table(values);
+  }
+
+  persona: Dictionary = {
+    firstName: 'John',
+    lastName: 'Doe',
+    dateOfBirth: '1985-05-15',
+    address: '123 Main Street, Anytown, USA',
+    phoneNumber: '123-456-7890',
+    email: 'john.doe@example.com',
+    gender: 'Male',
+    nationality: 'American',
+    maritalStatus: 'Single',
+    occupation: 'Software Engineer',
+  };
+
+  contratto: Dictionary = {
+    contractId: 'C12345',
+    contractType: 'Employment',
+    startDate: '2024-01-01',
+    endDate: '2025-01-01',
+    employerName: 'Tech Corp Inc.',
+    employeeName: 'John Doe',
+    salary: '50000',
+    benefits: 'Health, Dental, Vision',
+    jobTitle: 'Senior Developer',
+    jobDescription:
+      'Responsible for developing and maintaining web applications.',
+  };
 }
